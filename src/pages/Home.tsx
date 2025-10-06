4347b6aedf8a03b3e8e4212193aaab596feeaf50
GuruAswinidath <109160445+GuruAswinidath@users.noreply.github.com>
2025-10-06 15:30:26 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ const Home = () => {
       {/* Hero Section */}
       <section className="relative pt-32 pb-20 px-4 lg:px-8 overflow-hidden">
         <div
-          className="absolute inset-0 z-0 opacity-10"
+          className="absolute inset-0 z-0 opacity-7"
           style={{
             backgroundImage: `url(${heroImg})`,
             backgroundSize: 'cover',
